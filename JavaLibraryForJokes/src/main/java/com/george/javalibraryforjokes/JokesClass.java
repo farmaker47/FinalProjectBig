d3b9c34da005a283b01490dc9a3a03e3834dff5f
farmaker47 <farmaker47@yahoo.cm>
2018-03-28 17:02:11 +0300

    Added flavors.working w no depends

--- JavaLibraryForJokes/src/main/java/com/george/javalibraryforjokes/JokesClass.java
+++ JavaLibraryForJokes/src/main/java/com/george/javalibraryforjokes/JokesClass.java
@@ -21,9 +21,7 @@ public class JokesClass {
                 "Patrick: \"What school?\"";
         jokesForMentors[1] = "Sleep with an open window tonight!      1400 mosquitos like that. 420 mosquitos commented on it. 210 mosquitos shared this. " +
                 "One mosquito invited for the event. 2800 mosquitos will be attending the event.";
-        jokesForMentors[2] = "In a boomerang shop: \"I'd like to buy a new boomerang please. Also, can you tell me how to throw the old one away?\"\n" +
-                "\n" +
-                "Check out this really funny jokes: http://www.short-funny.com/funniest-jokes-3.php#ixzz5AxWZ95ya";
+        jokesForMentors[2] = "In a boomerang shop: \"I'd like to buy a new boomerang please. Also, can you tell me how to throw the old one away?";
     }
 
     public String randomJokesToPass(){
